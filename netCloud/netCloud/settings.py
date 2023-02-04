126783509a6e0a8c61429bc65a92bcfac1dd4928
王杭 <2381886606@qq.com>
2023-02-05 00:48:21 +0800

    修复setting文件

--- netCloud/netCloud/settings.py
+++ netCloud/netCloud/settings.py
@@ -50,6 +50,22 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'netCloud.urls'
 
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
 WSGI_APPLICATION = 'netCloud.wsgi.application'
 
 # Database
